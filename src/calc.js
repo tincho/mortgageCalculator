b507963e4f59d1955d3300e65c53baa738e1d32f
tincho <martin.salinas@gmail.com>
2019-02-05 12:25:43 -0300

    minimal test coverage and implementation of Calc module

---
+++ src/calc.js
@@ -0,0 +1,45 @@
+/**
+ * Principle & Interest based on Annuity formula
+ * @param {Object} calculatorState
+ * @param {number} calculatorState.interestRate
+ * @param {number} calculatorState.loanAmount
+ * @param {number} calculatorState.yearsOfMortgage
+ * @returns {number}
+ */
+const calculatePrincipleAndInterest = ({
+  interestRate,
+  loanAmount,
+  yearsOfMortgage,
+}) => {
+  const I = (interestRate / 100) / 12;
+  const Y = -yearsOfMortgage * 12;
+  const pow = (1 + I) ** Y;
+  return I * loanAmount / (1 - pow);
+};
+
+/**
+ * Arrange the values to show as results
+ * @param {Object} input
+ * @param {number} input.annualTax
+ * @param {number} input.annualInsurance
+ * @param {...number} calculatorState Rest of the values, relevant to calculatePrincipleAndInterest
+ * @see calculatePrincipleAndInterest
+ * @returns {Object} Calculated values
+ */
+window.calculateResults = function calculateResults({
+  annualTax,
+  annualInsurance,
+  ...calculatorState
+}) {
+  const principleAndInterest = calculatePrincipleAndInterest(calculatorState);
+  const monthlyTax = annualTax / 12;
+  const monthlyInsurance = annualInsurance / 12;
+  const totalMonthlyPayment = principleAndInterest + monthlyTax + monthlyInsurance;
+
+  return {
+    principleAndInterest,
+    monthlyTax,
+    monthlyInsurance,
+    totalMonthlyPayment,
+  };
+};
---
+++ test/calc.test.js
@@ -0,0 +1,33 @@
+const assert = require('assert');
+const calculateResults = require('../src/calc');
+
+describe('calc module', () => {
+  const sampleInput = {
+    interestRate: 8,
+    loanAmount: 100000,
+    yearsOfMortgage: 30,
+    annualTax: 1000,
+    annualInsurance: 300,
+  };
+
+  it('should return these 4 values', () => {
+    const q = calculateResults(sampleInput);
+    const expectedKeys = [
+      'monthlyInsurance',
+      'monthlyTax',
+      'principleAndInterest',
+      'totalMonthlyPayment',
+    ];
+    assert.deepEqual(expectedKeys, Object.keys(q).sort());
+  });
+
+  it('should divide annualInsurance by 12', () => {
+    const q = calculateResults(sampleInput);
+    assert.equal(q.monthlyInsurance, 25);
+  });
+
+  it('should divide annualTax by 12', () => {
+    const q = calculateResults(sampleInput);
+    assert.equal(Math.floor(q.monthlyTax), 83);
+  });
+});
